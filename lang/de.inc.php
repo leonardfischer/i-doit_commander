42fc2c8fde376446115a45df22dfd1bd8849c959
dev 0815 <post@leonardfischer.de>
2022-01-26 08:35:47 +0100

    Using placeholders to implement linebreaks

--- lang/de.inc.php
+++ lang/de.inc.php
@@ -7,7 +7,7 @@
 return [
     'LC__MODULE__LFISCHER_COMMANDER__COMMANDER'                    => 'Commander',
     'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_FOR_EXAMPLE'        => 'Zum Beispiel...',
-    'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_EXAMPLES'           => 'create object\ncreate server "my server"\nopen #123\nopen "* server"\nsearch "cloud"',
+    'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_EXAMPLES'           => 'create object[br]create server "my server"[br]open #123[br]open "* server"[br]search "cloud"',
     'LC__MODULE__LFISCHER_COMMANDER__COULD_NOT_FIND_OBJ_NAME'      => 'Es konnte kein Objekt mit dem Namen \"%s\" gefunden werden.',
     'LC__MODULE__LFISCHER_COMMANDER__COULD_NOT_FIND_OBJ_TYPE_NAME' => 'Es konnte kein Objekttyp mit dem Namen \"%s\" gefunden werden.',
     'LC__MODULE__LFISCHER_COMMANDER__DID_YOU_MEAN'                 => 'Mehrere Aktionen gefunden, meinten Sie...',
--- lang/en.inc.php
+++ lang/en.inc.php
@@ -7,7 +7,7 @@
 return [
     'LC__MODULE__LFISCHER_COMMANDER__COMMANDER'                    => 'Commander',
     'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_FOR_EXAMPLE'        => 'For example...',
-    'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_EXAMPLES'           => "create object\ncreate server 'my server'\nsearch 'cloud'",
+    'LC__MODULE__LFISCHER_COMMANDER__COMMANDER_EXAMPLES'           => "create object[br]create server 'my server'[br]search 'cloud'",
     'LC__MODULE__LFISCHER_COMMANDER__COULD_NOT_FIND_OBJ_NAME'      => 'Could not find any object with the name \"%s\".',
     'LC__MODULE__LFISCHER_COMMANDER__COULD_NOT_FIND_OBJ_TYPE_NAME' => 'Could not find any object type with the name \"%s\".',
     'LC__MODULE__LFISCHER_COMMANDER__DID_YOU_MEAN'                 => 'Found multiple actions, did you mean...',
